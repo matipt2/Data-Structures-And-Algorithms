e29311f25026afd03d01b9e328a09ab6b1fee61f
mathh1337 <112662623+matipt2@users.noreply.github.com>
2023-11-07 15:58:28 +0100

    update main.cpp

--- LIST/main.cpp
+++ LIST/main.cpp
@@ -1,142 +1,152 @@
 #include <iostream>
 using namespace std;
 
-#define MIN (-10000000) // Definiujemy stałą MIN.
+#define MIN (-10000000)
 
 class List {
 private:
-    int array[10]; // Tablica przechowująca elementy listy.
-    int size; // Aktualny rozmiar listy.
-    int last; // Indeks ostatniego elementu na liście.
+    int* array;
+    int size;
+    int last;
 
 public:
     List() {
-        size = 0; // Początkowo lista jest pusta, więc rozmiar wynosi 0.
-        last = -1; // Brak elementów w tablicy, więc last jest -1.
+        size = 0;
+        last = -1;
+        array = new int[10];
         for (int i = 0; i < 10; i++) {
-            array[i] = MIN; // Inicjalizujemy tablicę wartością MIN.
+            array[i] = MIN;
         }
     }
 
-    int first() {
-        if (size == 0) {
-            return -1; // Lista jest pusta.
-        }
-        return 0; // Zwracamy indeks pierwszego elementu.
+    ~List() {
+        delete[] array;
     }
 
-    int end() {
-        return last + 1; // Zwracamy indeks za ostatnim elementem.
-    }
 
-    int next(int p) {
+
+    int Next(int p) {
         if (p < 0 || p >= last) {
-            return -1; // Niepoprawna pozycja.
+            return -1;
         }
-        return p + 1; // Zwracamy indeks następnego elementu.
-    }
-
-    int previous(int p) {
-        if (p <= 0 || p > last) {
-            return -1; // Niepoprawna pozycja.
-        }
-        return p - 1; // Zwracamy indeks poprzedniego elementu.
-    }
-
-    bool Insert(int x, int p) {
-        if (size >= 10) {
-            return false; // Lista jest pełna.
-        }
-
-        if (p < 0 || p > last + 1) {
-            return false; // Niepoprawna pozycja.
-        }
-
-        for (int i = last; i >= p; i--) {
-            array[i + 1] = array[i]; // Przesuwamy elementy, aby zrobić miejsce dla nowego elementu.
-        }
-
-        array[p] = x; // Wstawiamy nowy element.
-        size++; // Zwiększamy rozmiar listy.
-        last++; // Aktualizujemy indeks ostatniego elementu.
-        return true; // Operacja zakończona sukcesem.
+        return p + 1;
     }
 
     bool Delete(int p) {
         if (p < 0 || p > last) {
-            return false; // Niepoprawna pozycja.
+            return false;
         }
 
         for (int i = p; i < last; i++) {
-            array[i] = array[i + 1]; // Usuwamy element przesuwając pozostałe elementy.
+            array[i] = array[i + 1];
         }
 
-        array[last] = MIN; // Ostatni element otrzymuje wartość MIN.
-        size--; // Zmniejszamy rozmiar listy.
-        last--; // Aktualizujemy indeks ostatniego elementu.
-        return true; // Operacja zakończona sukcesem.
+        array[last] = MIN;
+        size--;
+        last--;
+        return true;
+    }
+
+    int Previous(int p) {
+        if (p <= 0 || p > last) {
+            return -1;
+        }
+        return p - 1;
+    }
+
+    bool Insert(int x, int p) {
+        if (size >= 10) {
+            return false;
+        }
+
+        if (p < 0 || p > last + 1) {
+            return false;
+        }
+
+        for (int i = last; i >= p; i--) {
+            array[i + 1] = array[i];
+        }
+
+        array[p] = x;
+        size++;
+        last++;
+        return true;
     }
 
     int Locate(int x) {
         for (int i = 0; i <= last; i++) {
             if (array[i] == x) {
-                return i; // Znaleziono element.
+                return i;
             }
         }
-        return -1; // Element nie został znaleziony.
+        return -1;
+    }
+    int First() {
+        if (size == 0) {
+            return -1;
+        }
+        return 0;
+    }
+
+    int End() {
+        return last + 1;
     }
 
     int Retrieve(int p) {
         if (p >= 0 && p <= last) {
-            return array[p]; // Zwracamy element na danej pozycji.
+            return array[p];
         }
-        return MIN; // Niepoprawna pozycja lub brak elementu na danej pozycji.
-    }
-
-    void GetRidOffDuplicates() {
-        for (int i = 0; i <= last; i++) {
-            for (int j = i + 1; j <= last;) {
-                if (array[i] == array[j]) {
-                    Delete(j); // Usuwamy duplikat.
-                } else {
-                    j++; // Przechodzimy do następnego elementu.
-                }
-            }
-        }
+        return MIN;
     }
 };
-
 
 int main() {
     List myList;
 
-    // Test dla metody Insert
-    myList.Insert(10, 0); // Dodajemy 10 na pozycję 0
-    myList.Insert(20, 1); // Dodajemy 20 na pozycję 1
-    myList.Insert(30, 2); // Dodajemy 30 na pozycję 2
 
 
-    cout<<myList.Delete(2)<<endl;
-    for(int i=0;i<5;i++){
-        cout<<myList.Retrieve(i)<<endl;
+    myList.Insert(5, 0);
+    myList.Insert(16, 1);
+    myList.Insert(53, 2);
+    myList.Insert(23,3);
+    myList.Insert(432,4);
+
+
+    for (int i = 0; i < myList.End(); i++) {
+        cout << "elementy listy " << i << ": " << myList.Retrieve(i) << endl;
+    }
+    cout<<" "<<endl;
+    cout << "usuniecie pierwszego indexu: " << myList.Delete(1) << endl;
+    cout<<" "<<endl;
+
+    for (int i = 0; i < myList.End(); i++) {
+        cout << "elementy indexowane po i:  " << i << ": " << myList.Retrieve(i) << endl;
     }
 
-//    // Test dla metody Retrieve
-//    cout << myList.Retrieve(0) << endl; // Powinno wyświetlić 10
-//    cout << myList.Retrieve(1) << endl; // Powinno wyświetlić 20
-//    cout << myList.Retrieve(2) << endl; // Powinno wyświetlić 30
-//
-//    // Test dla metody Locate
-//    cout << myList.Locate(10) << endl; // Powinno wyświetlić 0
-//    cout << myList.Locate(20) << endl; // Powinno wyświetlić 1
-//    cout << myList.Locate(30) << endl; // Powinno wyświetlić 2
-//
-//    // Test dla metody Delete
-//    myList.Delete(1); // Usuwamy element na pozycji 1
-//
-//    // Test dla metody Retrieve po usunięciu elementu
-//    cout << myList.Retrieve(0) << endl; // Powinno wyświetlić 10
-//    cout << myList.Retrieve(1) << endl; // Powinno wyświetlić 30
+    cout<<" "<<endl;
+
+    cout << "53 jest na miejscu: " << myList.Locate(53) << endl;
+
+    cout<<" "<<endl;
+
+    cout << "pozycja po 0: " << myList.Next(0) << endl;
+
+    cout<<" "<<endl;
+
+    cout << "pozycja przed 2: " << myList.Previous(2) << endl;
+    cout<<" "<<endl;
+
+    cout << "pierwszy index: " << myList.First() << endl;
+    cout<<" "<<endl;
+
+    cout << "ostatni index: " << myList.End() << endl;
+    cout<<" "<<endl;
+
+
+    cout << "wartosc na indexie -1: " << myList.Retrieve(-1) << endl;
+    cout << "wartosc na indexie 0: " << myList.Retrieve(0) << endl;
+    cout << "wartosc na indexie 3: " << myList.Retrieve(3) << endl;
+    cout << "wartosc na indexie 5: " << myList.Retrieve(5) << endl;
 
     return 0;
 }
