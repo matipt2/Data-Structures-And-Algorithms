4691634ee5eda9416a11eb865b92072c436f5e96
mathh1337 <112662623+matipt2@users.noreply.github.com>
2023-11-13 00:05:42 +0100

    linkedlist

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,26 +1,155 @@
-typedef int element;
-typedef  struct cell
-{
-    element element;
-    cell * next;
-};
+#include <iostream>
 
-class Lista
-{
-protected:
-    cell * header; // głowa listy
+template <typename T>
+class cell {
 public:
-    Lista();    //konstruktor
-    ~Lista();  //destruktor
-    void Insert(element x, cell * p); // wstawia element x na pozycję next komórki o wskaźniku p
-    // (lub w przypadku pustej listy tworzy komórkę z elementem x w miejscu głowy listy)
-    void Delete(cell * p); // usuwa komórkę z pozycji next komórki o wskaźniku p
-    element Retrieve(cell * p); // zwraca element komórki o wskaźniku p
-    cell * Locate(element x); // zwraca wskaźnik do pierwszej komórki z elementem x
-    cell * First(); // zwraca wskaźnik do pierwszej komórki na liście
-    cell * Next(cell * p); // zwraca wskaźnik do kolejnej komórki po komórce o wskaźniku p
-    cell * Previous(cell * p); // zwraca wskaźnik do komórki poprzedzającej komórkę o wskaźniku p
-    cell * Last(); // zwraca wskaźnik do ostatniej komórki na liście
-
-    void print(); // wyświetla wszystkie elementy listy
+    T element;
+    cell *next;
 };
+
+template <typename T>
+class Lista {
+protected:
+    cell<T> *header;
+public:
+    Lista() {
+        header = nullptr;
+    }
+
+    ~Lista() {
+        while (header != nullptr) {
+            cell<T> *temp = header;
+            header = header->next;
+            delete temp;
+        }
+    }
+
+    void Insert(T x, cell<T> *p) {
+        cell<T> *newCell = new cell<T>;
+        newCell->element = x;
+        if (p == nullptr) {
+            newCell->next = header;
+            header = newCell;
+        } else {
+            newCell->next = p->next;
+            p->next = newCell;
+        }
+    }
+
+    bool Delete(cell<T> *p) {
+        if (p == nullptr || p->next == nullptr) {
+            return false;
+        }
+        cell<T> *temp = p->next;
+        p->next = p->next->next;
+        delete temp;
+        return true;
+    }
+
+    T Retrieve(cell<T> *p) {
+        if (p != nullptr && p->next != nullptr) {
+            return p->next->element;
+        }
+        return T();
+    }
+
+    cell<T> *Locate(T x) {
+        cell<T> *temp = header;
+        while (temp != nullptr) {
+            if (temp->element == x) {
+                return temp;
+            }
+            temp = temp->next;
+        }
+        return nullptr;
+    }
+
+    cell<T> *First() {
+        return header;
+    }
+
+    cell<T> *Next(cell<T> *p) {
+        if (p != nullptr) {
+            return p->next;
+        }
+        return nullptr;
+    }
+
+    cell<T> *Previous(cell<T> *p) {
+        if (p == nullptr || p == header) {
+            return nullptr;
+        }
+        cell<T> *temp = header;
+        while (temp != nullptr && temp->next != p) {
+            temp = temp->next;
+        }
+        return temp;
+    }
+
+    cell<T> *Last() {
+        cell<T> *temp = header;
+        while (temp != nullptr && temp->next != nullptr) {
+            temp = temp->next;
+        }
+        return temp;
+    }
+
+    void print() {
+        cell<T> *temp = header;
+        while (temp != nullptr) {
+            std::cout << temp->element << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+};
+int main() {
+    Lista<int> lista;
+
+    // Testujemy metodę Insert
+    lista.Insert(1, nullptr); // Dodajemy 1 na początek listy
+    lista.Insert(2, lista.First()); // Dodajemy 2 na drugą pozycję
+    lista.Insert(3, lista.Last()); // Dodajemy 3 na koniec listy
+
+    // Wyświetlamy listę, powinno wyjść: 1 2 3
+    lista.print();
+
+    // Testujemy metodę Delete
+    lista.Delete(lista.First()); // Usuwamy pierwszy element
+    lista.Delete(lista.Last()); // Usuwamy ostatni element
+
+    // Wyświetlamy listę, powinno wyjść: 2
+    lista.print();
+
+    // Testujemy metodę Retrieve
+    std::cout << "Element na pierwszej pozycji: " << lista.Retrieve(lista.First()) << std::endl;
+
+    // Testujemy metodę Locate
+    cell<int> *cell = lista.Locate(2);
+    if (cell != nullptr) {
+        std::cout << "Znaleziono element 2" << std::endl;
+    } else {
+        std::cout << "Nie znaleziono elementu 2" << std::endl;
+    }
+
+    // Testujemy metodę Next
+    cell = lista.Next(lista.First());
+    if (cell != nullptr) {
+        std::cout << "Następny element po pierwszym: " << cell->element << std::endl;
+    } else {
+        std::cout << "Brak następnego elementu po pierwszym" << std::endl;
+    }
+
+    // Testujemy metodę Previous
+    cell = lista.Previous(lista.First());
+    if (cell != nullptr) {
+        std::cout << "Poprzedni element przed pierwszym: " << cell->element << std::endl;
+    } else {
+        std::cout << "Brak poprzedniego elementu przed pierwszym" << std::endl;
+    }
+
+    // Testujemy metodę Last
+    std::cout << "Ostatni element na liście: " << lista.Last()->element << std::endl;
+
+    return 0;
+}
