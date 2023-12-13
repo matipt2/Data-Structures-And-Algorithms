23d52c51203a593cdc3a87b8dc1fc829833885fc
matipt2 <mateuszpt2@o2.pl>
2023-12-13 23:58:06 +0100

    binarytree

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -27,7 +27,6 @@ void insert(Node<T>*& root, T data) {
 template <typename T>
 void preorder(Node<T>* root) {
     if (root == nullptr) return;
-    std::cout << root->data << " ";
     preorder(root->left);
     preorder(root->right);
 }
@@ -36,7 +35,6 @@ template <typename T>
 void inorder(Node<T>* root) {
     if (root == nullptr) return;
     inorder(root->left);
-    std::cout << root->data << " ";
     inorder(root->right);
 }
 
@@ -45,45 +43,4 @@ void postorder(Node<T>* root) {
     if (root == nullptr) return;
     postorder(root->left);
     postorder(root->right);
-    std::cout << root->data << " ";
-}
-
-int main() {
-    Node<int>* root = nullptr;
-    insert(root, 12);
-    insert(root, 4);
-    insert(root, 7);
-    insert(root, 25);
-    insert(root, 8);
-    insert(root, 11);
-
-    std::cout << "Preorder: ";
-    preorder(root);
-    std::cout << "\n";
-
-    std::cout << "Inorder: ";
-    inorder(root);
-    std::cout << "\n";
-
-    std::cout << "Postorder: ";
-    postorder(root);
-    std::cout << "\n";
-
-    insert(root, 40);
-    insert(root, 3);
-    insert(root, 14);
-
-    std::cout << "Preorder po dodaniu: ";
-    preorder(root);
-    std::cout << "\n";
-
-    std::cout << "Inorder po dodaniu: ";
-    inorder(root);
-    std::cout << "\n";
-
-    std::cout << "Postorder po dodaniu: ";
-    postorder(root);
-    std::cout << "\n";
-
-    return 0;
 }
